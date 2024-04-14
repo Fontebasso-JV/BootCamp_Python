45ca7d330da7a79302023c89cb9757cd1aa4f718
João Fontebasso <joao.alvescont@gmail.com>
2024-04-14 02:22:43 -0300

    novo exercicio

--- aula_04/exercicios.py
+++ aula_04/exercicios.py
@@ -1,4 +1,3 @@
-""" 
 controle_biblioteca: dict = {
    "Livro_01": { "Titulo": "Game Of Thrones",
     "Autor": "Estagiário",
@@ -27,21 +26,20 @@ print(controle_biblioteca2.values)
 lista_compras = ["maçã", "banana", "cereja"]
 precos = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
 total = sum(precos[key] for key in lista_compras)
-print(f"Preço total: {total}") """
+print(f"Preço total: {total}") 
 
 
-""" idades = [22, 15, 30, 17, 18]
+idades = [22, 15, 30, 17, 18]
 
 idade_filtrada = []
 for i in idades:
     if i >= 18:
         idade_filtrada.append(i)
 
-print(idade_filtrada) """
+print(idade_filtrada) 
 
 
-
-""" produtos = [
+produtos = [
     {"id": 1, "nome": "Teclado", "preço": 100},
     {"id": 2, "nome": "Mouse", "preço": 80},
     {"id": 3, "nome": "Monitor", "preço": 300}
@@ -55,19 +53,34 @@ for i in produtos:
     if i["id"] == 2:
         i["preço"] = 90
 
-print(produtos) """
-
-
-""" estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
-
-for produto in estoque.values():
-    if produto > 0:
-        print(produto) """
+print(produtos) 
 
 
 estoque = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
 
-estoque_positivo = {produto: quantidade for produto, quantidade in estoque.items() if quantidade > 0}
-estoque2 = {produto: quantidade in estoque.items()}
+for produto in estoque.values():
+    if produto > 0:
+        print(produto) 
 
-print(estoque_positivo)
+ 
+
+#Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
+
+sequencia = [1,4,8,12]
+
+def somar_numeros_emlista(lista):
+    soma = 0
+    for i in lista:
+        soma += i
+
+    return soma
+
+resultado = somar_numeros_emlista(sequencia)
+
+print(resultado)
+
+
+#Crie uma função que receba um número como argumento e retorne True se o número for primo e False caso contrário.
+
+
+
