ed4c1dbd674ce8ed7414e047b4ccecd59b68e10e
João Fontebasso <joao.alvescont@gmail.com>
2024-04-21 11:47:40 -0300

    aulas

--- aula_poo/csv_tst.py
+++ aula_poo/csv_tst.py
@@ -6,27 +6,3 @@ df_filtrado = df[df["estado"] == 'SP']
 
 print(df)
 print(df_filtrado)
-
-
-class CsvProcessor:
-    def __init__(self,file_path: str):
-        self.file_path = file_path
-        self.df = None
-
-
-        def carregar_csv(self):
-            self.df = pd.read_csv(self.file_path)
-
-
-        
-        def filtrar_por(self,collumn,atributo):
-            return self.df[self.df[collumn] == atributo]
-        
-
-arquivo_csv = './exemplo.csv'
-filtro = 'estado'
-limite = 'SP'
-
-arquivo_CSV = CsvProcessor(arquivo_csv)
-arquivo_CSV.carregar_csv()
-print(arquivo_CSV.filtrar_por(filtro,limite))
